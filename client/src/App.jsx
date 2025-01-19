43a5137a24bcb287c603557024c3019d1575fbcd
Alok Kumar <alok87573770@gmail.com>
2025-01-19 22:44:31 +0530

    Create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className='text-red-500 flex-col'>App</div>
-  )
+    <BrowserRouter>
+    
+    <Header />{/* header */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import {Link} from 'react-router-dom';
+export default function header() {
+  return (
+    <div className='bg bg-slate-200'>
+       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 " >
+       <Link to ='/'>
+       <h1 className='font-bold'>Header</h1>
+       </Link>
+       <ul className='flex gap-4'>
+        
+        <Link to="/">
+        <li>Home</li>
+        </Link>
+
+        <Link to="/">
+        <li>About</li>
+        </Link>
+
+        <Link to="/">
+        <li>Profile</li>
+        </Link>
+
+        <Link to="/">
+        <li>Signin</li>
+        </Link>
+        
+        <Link to="/">
+        <li>Signup</li>
+        </Link>
+        
+        
+       
+        
+        
+       </ul>  
+       </div>
+    </div>
+  )
+}
+
